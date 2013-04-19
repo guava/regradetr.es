07ce347e0092c671b46438fc1b88bdf695488315
Guilherme Carvalho <guilherme@guava.com.br>
2013-04-19 14:43:57 -0300

    First beta release of regradetr.es

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+function CrossMultiplicationCtrl($scope) {
+  $scope.a = undefined;
+  $scope.b = undefined;
+  $scope.c = undefined;
+
+  $scope.d = function() {
+    if(!$scope.checkValidity($scope.a))
+      return '';
+    if(!$scope.checkValidity($scope.b))
+      return '';
+    if(!$scope.checkValidity($scope.c))
+      return '';
+
+    var d = ($scope.parseNumber($scope.c) * $scope.parseNumber($scope.b)) / $scope.parseNumber($scope.a);
+
+    return $scope.numberToString(d);
+  };
+
+  $scope.checkValidity = function(value) {
+    if(value === undefined)
+      return false;
+
+    return $scope.parseNumber(value);
+  };
+
+  $scope.parseNumber = function(value) {
+    var value = value.replace(',', '.');
+
+    return parseFloat(value);
+  }
+
+  $scope.numberToString = function(number) {
+    var strValue = number + '';
+
+    return strValue.replace('.', ',').substr(0, 6);
+  }
+}
