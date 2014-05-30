6453e67186f4669522e8c3c79748fddcf54b5488
Guilherme Carvalho <guilherme@guava.com.br>
2014-05-30 18:10:32 -0300

    Really working number-only fields.

--- public/js/main.js
+++ public/js/main.js
@@ -72,14 +72,9 @@ function CrossMultiplicationCtrl($scope) {
 }
 
 $('.input-1, .input-2, .input-3').on('keypress', function(event) {
-  var controlKeys = [8, 9, 13, 35, 36, 37, 39,44,110,190];
-  var isControlKey = controlKeys.join(",").match(new RegExp(event.which));
+  var validKeys = "-1234567890,"
 
-  if (!event.which || // Control keys in most browsers. e.g. Firefox tab is 0
-      event.metaKey ||
-      (48 <= event.which && event.which <= 57) || // Always 0 through 9
-      (96 <= event.which && event.which <= 106) || // Always 0 through 9 from number section
-      isControlKey) { // Opera assigns values for control keys.
+  if (!event.which || event.metaKey || (validKeys.indexOf(event.key)) >= 0) {
     return;
   } else {
     event.preventDefault();
